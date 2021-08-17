81752a7e9a0592440515ef7b944dcfec12d93eee
maulanas <maulana@troopergears.com>
2021-08-17 17:27:09 +0700

    Add sound when clicked

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
-const handleClick = () => {
-    alert(`I got clicked!`)
-}
+// const handleClick = () => {
+//     let tom1Sound = new Audio("./sounds/tom-1.mp3")
+//     tom1Sound.play();
+// }
 
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick)
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        let buttonInnerHTML = this.innerHTML;
+        
+        switch (buttonInnerHTML) {
+            case "w":
+                let tom1 = new Audio("./sounds/tom-1.mp3")
+                tom1.play();
+                break;
+
+            case "a":
+                let tom2 = new Audio("./sounds/tom-2.mp3")
+                tom2.play();
+                break;
+
+            case "s":
+                let tom3 = new Audio("./sounds/tom-3.mp3")
+                tom3.play();
+                break;
+            
+            case "d":
+                let tom4 = new Audio("./sounds/tom-4.mp3")
+                tom4.play();
+                break;
+
+            case "j":
+                let snare = new Audio("./sounds/snare.mp3")
+                snare.play();
+                break;
+
+            case "k":
+                let crash = new Audio("./sounds/crash.mp3")
+                crash.play();
+                break;
+
+            case "l":
+                let kick = new Audio("./sounds/kick-bass.mp3")
+                kick.play();
+                break;
+
+            default: console.log(`${buttonInnerHTML} The default case is trigerred`)
+        }
+        // this.style.color = "#DBEDF3"
+    })
 }
 
