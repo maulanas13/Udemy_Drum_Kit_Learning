53ec2d94b09c9112a9a7b3352c427df0a1226a63
maulanas <maulana@troopergears.com>
2021-08-17 16:20:36 +0700

    Test function to all buttons

---
+++ index.js
@@ -0,0 +1,8 @@
+const handleClick = () => {
+    alert(`I got clicked!`)
+}
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick)
+}
+
