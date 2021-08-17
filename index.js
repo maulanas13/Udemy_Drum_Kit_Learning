207b827fb94158168b76a77112b8384128cb886e
maulanas <maulana@troopergears.com>
2021-08-17 22:25:54 +0700

    Add keydown event listener to index.js

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-// const handleClick = () => {
-//     let tom1Sound = new Audio("./sounds/tom-1.mp3")
-//     tom1Sound.play();
-// }
+const tom1 = () => {
+    let tom1 = new Audio("./sounds/tom-1.mp3")
+    tom1.play();
+}
+
+const tom2 = () => {
+    let tom2 = new Audio("./sounds/tom-2.mp3")
+    tom2.play();
+}
+
+const tom3 = () => {
+    let tom3 = new Audio("./sounds/tom-3.mp3")
+    tom3.play();
+}
+
+const tom4 = () => {
+    let tom4 = new Audio("./sounds/tom-4.mp3")
+    tom4.play();
+}
+
+const snare = () => {
+    let snare = new Audio("./sounds/snare.mp3")
+    snare.play();
+}
+
+const crash = () => {
+    let crash = new Audio("./sounds/crash.mp3")
+    crash.play();
+}
+
+const kick = () => {
+    let kick = new Audio("./sounds/kick-bass.mp3")
+    kick.play();
+}
+
+document.addEventListener("keydown", function(event) {
+    let keyValue = event.key
+    keyValue === "w" ? tom1()
+    : keyValue === "a" ? tom2()
+    : keyValue === "s" ? tom3()
+    : keyValue === "d" ? tom4()
+    : keyValue === "j" ? snare()
+    : keyValue === "k" ? crash()
+    : keyValue === "l" ? kick()
+    : console.log(`Key pressed wrong`);
+})
 
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
@@ -9,38 +51,31 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
         
         switch (buttonInnerHTML) {
             case "w":
-                let tom1 = new Audio("./sounds/tom-1.mp3")
-                tom1.play();
+                tom1();
                 break;
 
             case "a":
-                let tom2 = new Audio("./sounds/tom-2.mp3")
-                tom2.play();
+                tom2();
                 break;
 
             case "s":
-                let tom3 = new Audio("./sounds/tom-3.mp3")
-                tom3.play();
+                tom3()
                 break;
             
             case "d":
-                let tom4 = new Audio("./sounds/tom-4.mp3")
-                tom4.play();
+                tom4()
                 break;
 
             case "j":
-                let snare = new Audio("./sounds/snare.mp3")
-                snare.play();
+                snare();
                 break;
 
             case "k":
-                let crash = new Audio("./sounds/crash.mp3")
-                crash.play();
+                crash();
                 break;
 
             case "l":
-                let kick = new Audio("./sounds/kick-bass.mp3")
-                kick.play();
+                kick();
                 break;
 
             default: console.log(`${buttonInnerHTML} The default case is trigerred`)
